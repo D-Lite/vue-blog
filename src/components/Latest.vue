7c6ec9bd378e19fe0df7ecd14c62dfdb3d62b2fa
Olabemiwo Daniel O <olabemiwodaniel@gmail.com>
2021-01-20 14:37:26 +0100

    More designs

--- src/components/Latest.vue
+++ src/components/Latest.vue
@@ -2,8 +2,32 @@
     <section class="latest">
         <h3>Latest Posts</h3>
         <div class="posts">
+            <div class="post">
+                <div class="image" :style="{ backgroundImage: `url(${require('../assets/image1.jpg')})` }"></div>
+                <h4>Plan Ahead</h4>
+            </div>
+            <div class="post col-2 row-2">
+                <div class="image" :style="{ backgroundImage: `url(${require('../assets/image2.jpg')})` }"></div>
+                <h4>Vue JS fundamentals</h4>
+            </div>
+            <div class="post row-2">
+                <div class="image" :style="{ backgroundImage: `url(${require('../assets/image3.jpg')})` }"></div>
+                <h4>Everything wrong with AI </h4>
+            </div>
+            <div class="post row-2">
+                <div class="image" :style="{ backgroundImage: `url(${require('../assets/image4.jpg')})` }"></div>
+                <h4>#100daysofcode</h4>
+            </div>
+            <div class="post">
+                <div class="image" :style="{ backgroundImage: `url(${require('../assets/image5.jpg')})` }"></div>
+                <h4>NODE package manager</h4>
+            </div>
+            <div class="post col-2">
+                <div class="image" :style="{ backgroundImage: `url(${require('../assets/image6.jpg')})` }"></div>
+                <h4>Take a break</h4>
+            </div>
+        </div>  
 
-        </div>
     </section>
 </template>
 
@@ -24,16 +48,41 @@ export default {
         display: grid;
         // grid-template-columns: 15px  100px  100px  15px;
         grid-template-columns: repeat(4, 1fr);
-        grid-template-rows: repeat(3, 195px);
-        grid-gap: 25px;
+        grid-template-rows: repeat(3, 196px);
+        grid-gap: 30px;
 
         margin: 15px;
 
-        .post {
-            
+        .image {
+            flex: 1;
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
         }
 
+        .post {
+            display: flex;
+            flex-direction: column;
 
+            background-color: #ffffff;
+            box-shadow: 3px 3px 8px 0px rgba(0, 0, 0, 0.2); 
+
+            h3{
+                box-sizing: border-box;
+                padding: 3px 15px 0px 15px;
+                height: 50px;
+                line-height: 50px;
+                margin: 0px;
+            }
+
+            &.col-2{
+                grid-column: span 2;        
+            }            
+            &.row-2{
+                grid-row: span 2;
+            }
+
+        }
     }
 }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="posts">
+        <button class="load">See more</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.posts {
+    width: 100%;
+    max-width: 1280px;
+    margin: 50px auto;
+
+    .load {
+        display: block;
+        width: 100%;
+        max-width: 250px;
+        height: 50px;
+
+        margin: 0 auto;
+
+        outline: none;
+        border: none;
+        border-radius: 10px;
+        appearance: none;
+
+        background-color: #ffce00;
+        box-shadow: 3px 3px 8px 3px rgba(0, 0, 0, 0.2);
+        box-sizing: border-box;
+
+        color: #171717;
+        font-family: 'Rockwell', serif;
+        font-size: 28px;
+        font-weight: 700;
+    }
+}
+</style>
